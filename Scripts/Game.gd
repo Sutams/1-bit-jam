a830cfa5aa375a6603a03b74e4c1607d1e91547e
Sutams <ernestogarciaa1@gmail.com>
2026-01-09 21:43:27 -0300

    Day 4 progress (chest, spikes, light rotation)

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,10 +1,102 @@
 extends Node
 
+var rect
+var clam_open = true
+var chest_open = true
+
+# Do this for the other variables!
+@onready var fog_sprite = $FogSprite
+@onready var map = $NavigationRegion2D
+@onready var player = $Player
+var fog_image : Image
+var vision_image : Image
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	rect = $NavigationRegion2D.get_bounds()
+	#generate_fog()
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _physics_process(_delta: float) -> void:
+	##if player.velocity.length():
+		##update_fog()
+	#pass
+#
+#
+#func generate_fog()-> void:
+	##var fog_size = 16
+	#var map_size_x = (rect.end.x-rect.position.x)*16
+	#var map_size_y = (rect.end.y-rect.position.y)*16
+	#
+	#fog_image = Image.create(map_size_x, map_size_y, false, Image.Format.FORMAT_RGBAH)
+	#fog_image.fill(Color.DIM_GRAY)
+	#
+	#fog_sprite.texture = ImageTexture.create_from_image(fog_image)
+	#
+	#vision_image = player.vision_sprite.texture.get_image()
+	#vision_image.convert(Image.Format.FORMAT_RGBAH)
+#
+#func update_fog()-> void:
+	#var vision_rect = Rect2(Vector2.ZERO, vision_image.get_size())
+	#fog_image.blend_rect(fog_image,vision_rect, player.global_position)
+	#var fog_texture = ImageTexture.create_from_image(fog_image)
+	#fog_sprite.texture = fog_texture
+
+func fish_path(body: Node2D) -> void:
+	if body.name == "Fish":
+		$Goal.global_position = Vector2(randf_range(rect.position.x,rect.end.x),
+										randf_range(rect.position.y,rect.end.y))
+		body.make_path()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _on_goal_body_entered(body: Node2D) -> void:
+	fish_path(body)
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		$Fish.run_from_player(body.global_position)
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		await get_tree().create_timer(1.0).timeout
+		$Fish.rest()
+		fish_path($Fish)
+
+
+func _on_clam_timer_timeout() -> void:
+	if clam_open:
+		$Clam/CollisionShape2D.set_deferred("disabled", false)
+		$Clam/AnimatedSprite2D.play("Close")
+		if $Clam/GemArea:
+			$Clam/GemArea/Sprite2D.visible = false
+			$Clam/GemArea/PointLight2D.enabled = false
+			$Clam/GemArea/PointLight2D2.enabled = false
+	else: #Open clam
+		$Clam/CollisionShape2D.set_deferred("disabled", true)
+		$Clam/AnimatedSprite2D.play("Open")
+		if $Clam/GemArea:
+			$Clam/GemArea/Sprite2D.visible = true
+			$Clam/GemArea/PointLight2D.enabled = true
+			$Clam/GemArea/PointLight2D2.enabled = true
+	clam_open = !clam_open
+
+
+func _on_chest_timer_timeout() -> void:
+	if chest_open:
+		$Chest/CollisionShape2D.set_deferred("disabled", false)
+		$Chest/AnimatedSprite2D.play("Close")
+		if $Chest/GemArea:
+			$Chest/GemArea/Sprite2D.visible = false
+			$Chest/GemArea/PointLight2D.enabled = false
+			$Chest/GemArea/PointLight2D2.enabled = false
+	else: #Open chest
+		$Chest/CollisionShape2D.set_deferred("disabled", true)
+		$Chest/AnimatedSprite2D.play("Open")
+		if $Chest/GemArea:
+			$Chest/GemArea/Sprite2D.visible = true
+			$Chest/GemArea/PointLight2D.enabled = true
+			$Chest/GemArea/PointLight2D2.enabled = true
+	chest_open = !chest_open
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,15 +1,39 @@
 extends CharacterBody2D
 
-const SPEED = 70.0
-const DEACC = 0.8
+const speed = 75.0
+const deacc = 2
 const init_light_deg = 180
 var light_deg = init_light_deg
 var weights = 2.3
+var health = 3
+var took_damage = false
+var rotation_speed = 30
+
+@onready var vision_sprite = $VisionSprite
 
 func _ready() -> void:
 	$AnimatedSprite2D.flip_h = false
+	$PointLight2D.rotation_degrees = 180
+
+func damage():
+	health -=1
+	print("-1hp")
+	if health > 0:
+		await get_tree().create_timer(1.5).timeout
+		took_damage = false
+		return
+	print("Game over")
+	get_tree().call_deferred("reload_current_scene")
 
 func _physics_process(delta: float) -> void:
+	for i in get_slide_collision_count():
+		var coll = get_slide_collision(i)
+		if coll.get_collider().name == "Spikes":
+			if !took_damage:
+				took_damage = !took_damage
+				damage()
+
+	
 	if Input.is_action_just_pressed("drop"):
 		if weights > 1:
 			weights -= 1
@@ -18,38 +42,51 @@ func _physics_process(delta: float) -> void:
 		if weights < 3:
 			weights += 1
 	
+	
+	var move_x := Input.get_axis("left", "right")
+	if move_x:
+		velocity.x = move_x * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, deacc)
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var move_y := Input.get_axis("up", "down")
 	if move_y:
-		velocity.y = move_y * SPEED
+		velocity.y = move_y * speed
+		light_deg = move_toward(light_deg, 90, 30)
 		#light_deg = move_toward(light_deg, light_deg+move_y, 1)
 	else:
-		velocity.y = move_toward(velocity.y, 0, DEACC)
-	
-	var move_x := Input.get_axis("left", "right")
-	if move_x:
-		velocity.x = move_x * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, DEACC)
+		velocity.y = move_toward(velocity.y, 0, deacc)
 	
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta * 1/3
+		velocity += (get_gravity() * delta * weights)/3
 	
 	if velocity.length() > 0:
 		$AnimatedSprite2D.play("Walk")
 	else:
 		$AnimatedSprite2D.play("Idle")
-		
+	
+	var target_angle = velocity.angle()+PI
 	if velocity.x > 0:
 		$AnimatedSprite2D.flip_h = false
-		light_deg = move_toward(light_deg, 180, 30)
 	if velocity.x < 0:
 		$AnimatedSprite2D.flip_h = true
-		light_deg = move_toward(light_deg, 360, 30)
+	if velocity == Vector2.ZERO:
+		if $AnimatedSprite2D.flip_h:
+			target_angle = 0
+		else:
+			target_angle = PI
+			
+	$PointLight2D.rotation = lerp_angle($PointLight2D.rotation, target_angle, delta * rotation_speed)
 	
-	$PointLight2D.rotation_degrees = light_deg
-	$LightOccluder2D.rotation_degrees = light_deg
-	
+	#$PointLight2D.rotation_degrees = light_deg
 	move_and_slide()
+	#queue_redraw()
+
+#func _draw():
+	
+	#var facing = Vector2.RIGHT.rotated(rotation)
+	#draw_line(Vector2.ZERO, velocity*10, Color.AQUA, 3.0)
+	
+	#draw_circle(,10,Color.BLUE,)
